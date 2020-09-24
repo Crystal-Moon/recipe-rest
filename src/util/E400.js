730f905e712f335c676782964790077ddbdeab75
perla.stto <perla.stto@gmail.com>
2020-09-24 15:21:20 -0300

    apply style guide Airbnb

--- src/util/E400.js
+++ src/util/E400.js
@@ -1,30 +1,30 @@
 export default {
 	BAD_LOGIN: {
-		es: "Los datos ingresados son incorrectos.",
-		en: "Incorret credentials"
+		es: 'Los datos ingresados son incorrectos.',
+		en: 'Incorret credentials'
 	},
 	BAD_TOKEN: {
-		es: "Inicie sesion nuevamente.",
-		en: "Login again please."
+		es: 'Inicie sesion nuevamente.',
+		en: 'Login again please.'
 	},
 	EXISTS: {
-		es: "El correo ya existe.",
-		en: "The user already exists."
+		es: 'El correo ya existe.',
+		en: 'The user already exists.'
 	},
 	NOT_FOUND: {
-		es: "No encontrado.",
-		en: "Not found."
+		es: 'No encontrado.',
+		en: 'Not found.'
 	},
 	NOT_PERMISSION: {
-		es: "No tienes permisos para realizar esta acción.",
-		en: "Don't have permissions to this action."
+		es: 'No tienes permisos para realizar esta acción.',
+		en: 'Don\'t have permissions to this action.'
 	},
 	NOT_REGISTER: {
-		es: "El usuario no esta registrado.",
-		en: "User not register."
+		es: 'El usuario no esta registrado.',
+		en: 'User not register.'
 	},
 	TOKEN_NOT_FOUND: {
-		es: "No se encuentra 'x-token' en las cabeceras",
-		en: "'x-token' headers not found."
+		es: 'No se encuentra "x-token" en las cabeceras',
+		en: '"x-token" headers not found.'
 	}
 }
